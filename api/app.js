43e12df487286f1da607da2b067c4655558e8e30
AugustoCorbalan <auguscorbi@gmail.com>
2023-04-20 14:50:51 -0300

    deleted cors restriccions

--- api/app.js
+++ api/app.js
@@ -4,9 +4,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser")
 const app = express();
 require('dotenv').config();
-// app.use(cors({ origin: ["http://localhost:3000", "https://portfolio-liard-mu-69.vercel.app"] }))
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", router);
 
-app.listen(PORT, console.log(`Listening on port ${PORT}`));
+app.listen(process.env.PORT, console.log(`Listening on port ${process.env.PORT}`));
