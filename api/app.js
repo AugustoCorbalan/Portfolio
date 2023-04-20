04286075bef0a6bdc73ff4c88d1ede69a974d851
AugustoCorbalan <auguscorbi@gmail.com>
2023-04-20 14:21:08 -0300

    fixed port

--- api/app.js
+++ api/app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = require("./routes/index.js");
 const cors = require("cors");
 const bodyParser = require("body-parser")
-
+const dotenv = require('dotenv')
 const app = express();
-
+dotenv.config();
 // app.use(cors({ origin: ["http://localhost:3000", "https://portfolio-liard-mu-69.vercel.app"] }))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", router);
 
-app.listen("3001", console.log("Listening on port 3001"));
+app.listen(PORT, console.log(`Listening on port ${PORT}` ));
