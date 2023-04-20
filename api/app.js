ac59c032e778ab5383a67735f7e318cea93c1be4
AugustoCorbalan <auguscorbi@gmail.com>
2023-04-20 14:39:34 -0300

    var port

--- api/app.js
+++ api/app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const router = require("./routes/index.js");
 const cors = require("cors");
 const bodyParser = require("body-parser")
-const dotenv = require('dotenv')
 const app = express();
-dotenv.config();
+require('dotenv').config();
 // app.use(cors({ origin: ["http://localhost:3000", "https://portfolio-liard-mu-69.vercel.app"] }))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", router);
 
-app.listen(PORT, console.log(`Listening on port ${PORT}` ));
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
